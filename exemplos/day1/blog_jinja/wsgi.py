fbe37c73856fdeebdce13b118c950ec363f3e317
Carlos Quintanilha <cquinta@gmail.com>
2023-09-16 12:11:26 -0300

    Framework amtq template

--- exemplos/day1/blog_jinja/wsgi.py
+++ exemplos/day1/blog_jinja/wsgi.py
@@ -1,73 +1,56 @@
-import cgi
-from database import conn
-from jinja2 import Environment, FileSystemLoader
 
-env=env = Environment(loader=FileSystemLoader("templates"))
+from database import conn
+from amtq  import Amtq
+
+app = Amtq()
+@app.route("^/$", template="list.template.html")
+def post_list():
+    posts = get_posts_from_database()
+    return {"post_list": posts}
+
+
+@app.route("^/(?P<id>\d{1,})$", template="post.template.html")
+def post_detail(id):
+    post = get_posts_from_database(post_id=id)[0]
+    return {"post": post}
+
+
+@app.route("^/new$", template="form.template.html")
+def new_post_form():
+    return {}
+
+
+@app.route("^/new$", method="POST")
+def new_post_add(form):
+    post = {item.name: item.value for item in form.list}
+    add_new_post(post)
+    return "New post Created with Success!", "201 Created", "text/plain"
+
 
 def get_posts_from_database(post_id=None):
     cursor = conn.cursor()
-    fields = ["id", "title", "content", "author"]
+    fields = ("id", "title", "content", "author")
+
     if post_id:
-        results = cursor.execute(f"SELECT * FROM posts WHERE id = ?;", (post_id,))
+        results = cursor.execute("SELECT * FROM post WHERE id = ?;", post_id)
     else:
-        results = cursor.execute("SELECT * FROM posts;")
-    return [dict(zip(fields, row)) for row in results]
+        results = cursor.execute("SELECT * FROM post;")
 
-
-def render_template(template_name, **context):
-    template = env.get_template(template_name)
-    return template.render(**context).encode("utf-8")   
-
+    return [dict(zip(fields, post)) for post in results]
 
 
 def add_new_post(post):
     cursor = conn.cursor()
     cursor.execute(
         """\
-            INSERT INTO posts (title, content, author) 
-            values(:title, :content, :author)
+        INSERT INTO post (title, content, author)
+        VALUES (:title, :content, :author);
         """,
         post,
     )
     conn.commit()
 
 
-def application(environ, start_response):
-    body = b"Content Not Found"
-    status = "404 Not Found"
-    path = environ.get("PATH_INFO")
-    method = environ.get("REQUEST_METHOD")
-
-    # Roteamento de URLs
-    if path == "/" and method == "GET":
-        posts = get_posts_from_database()
-        body = render_template("list.template.html", post_list=posts)
-        status = "200 OK"
-    elif path.split("/")[-1].isdigit() and method == "GET":
-        post_id = path.split("/")[-1]
-        body = render_template(
-            "post.template.html", post=get_posts_from_database(post_id=post_id)[0]
-        )
-        status = "200 OK"
-    elif path == "/new" and method == "GET":
-        body = render_template("form.template.html")
-        status = "200 OK"
-    elif path == "/new" and method == "POST":
-        form = cgi.FieldStorage(
-            fp=environ["wsgi.input"], environ=environ, keep_blank_values=True
-        )
-        post = {item.name: item.value for item in form.list}
-        add_new_post(post)
-        body = b"New post created with success"
-        status = "201 Created"
-
-    headers = [("Content-type", "text/html")]
-    start_response(status, headers)
-    return [body]
-
-
 if __name__ == "__main__":
-    from wsgiref.simple_server import make_server
+    app.run()
 
-    server = make_server("0.0.0.0", 8000, application)
-    server.serve_forever()
